599516acdb501f34e7e3a2d91e4c7b3b654cd6b5
Jeff Mather <jeff@reliantsoftwareeng.com>
2023-11-01 17:05:31 -0700

    Use an environment variable for whether the database connection should use SSL

--- data-access/use-data-client.ts
+++ data-access/use-data-client.ts
@@ -7,5 +7,6 @@ export function useDataClient(): Client {
     host: env.POSTGRES_HOST,
     database: env.POSTGRES_DATABASE,
     password: env.POSTGRES_PASSWORD,
+    ssl: env.USE_SSL === "true",
   });
 }
